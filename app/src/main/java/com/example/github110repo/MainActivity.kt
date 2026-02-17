c7c75888d8300f3dec93e53201241d29982f0078
SUDIPBANIK <Sudipbanik42@gmail.com>
2026-02-17 17:05:03 +0600

    new changed

--- app/src/main/java/com/example/github110repo/MainActivity.kt
+++ app/src/main/java/com/example/github110repo/MainActivity.kt
@@ -12,36 +12,60 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.github110repo.ui.theme.Github110RepoTheme
-
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.material3.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Github110RepoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+
         }
     }
 }
+//@Composable
+//fun CenteredTextColumn() {
+//    Column(
+//        modifier = Modifier
+//            .fillMaxSize()
+//            .padding(16.dp),
+//
+//        verticalArrangement = Arrangement.Bottom,
+//        horizontalAlignment = Alignment.CenterHorizontally
+//    ) {
+//        Text(
+//            text = "Mercantile Bank",
+//            style = MaterialTheme.typography.headlineMedium
+//        )
+//    }
+//}
+
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CenteredTextRow() {
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalArrangement = Arrangement.Center
+
+    ) {
+        Text(
+            text = "Mercantile Bank",
+            style = MaterialTheme.typography.headlineMedium
+        )
+    }
 }
 
-@Preview(showBackground = true)
+
+
+
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
-    Github110RepoTheme {
-        Greeting("Android")
-    }
+
+
 }
